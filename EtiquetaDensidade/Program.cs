52ae5649088ebb4ffed7ec6d526bc7fde660a153
Guilheme Augusto Vieira <guilhermeavdino@gmail.com>
2025-05-13 20:12:15 -0300

    feat: windows-publish

--- EtiquetaDensidade/Program.cs
+++ EtiquetaDensidade/Program.cs
@@ -1,35 +1,23 @@
 ﻿Console.WriteLine(" ======= Localizar e Substituir Etiqueta ======== ");
 Console.WriteLine();
 
-Console.WriteLine("👉🏻 Insira o que será buscado:");
+Console.WriteLine(" ==> Insira o que será buscado:");
 var findValue = Console.ReadLine();
 
-Console.WriteLine($"👀 Confirma o valor: [{findValue}]?");
-var confirmValue = Console.ReadLine();
-confirmValue ??= string.Empty;
-
-if (!confirmValue.Equals("s", StringComparison.InvariantCultureIgnoreCase))
-    return;
-
 Console.WriteLine();
-Console.WriteLine("👉🏻 Insira o novo valor:");
+Console.WriteLine(" ==> Insira o novo valor:");
 var replaceValue = Console.ReadLine();
-
-Console.WriteLine($"👀 Confirma o valor: [{replaceValue}]?");
-confirmValue = Console.ReadLine();
-confirmValue ??= string.Empty;
-
-if (!confirmValue.Equals("s", StringComparison.InvariantCultureIgnoreCase))
-    return;
     
 Console.WriteLine();
 var allFiles = Directory.GetFiles(".", "*.fmt", SearchOption.TopDirectoryOnly);
+
 var numberOfFiles = allFiles.Length;
 var numberofModifiedFiles = 0;
+var logLines = new List<string>();
 
 if (numberOfFiles == 0)
 {
-    Console.WriteLine($"😿 Não foram encontrados arquivos FMT neste diretório!");
+    Console.WriteLine($" ==> Não foram encontrados arquivos FMT neste diretório!");
     return;
 }
 
@@ -40,6 +28,8 @@ for (var i = 0; i < numberOfFiles; i++)
     
     var newFileLines = new List<string>();
     var modifiedFile = false;
+    string logLine;
+    
     foreach (var line in fileLines)
     {
         if (!line.StartsWith(findValue ?? string.Empty))
@@ -56,15 +46,24 @@ for (var i = 0; i < numberOfFiles; i++)
     {
         File.WriteAllLines(file, newFileLines);
         numberofModifiedFiles++;
-        Console.WriteLine($"👉🏻 Arquivo Alterado: ${file}");
+        
+        logLine = $" ==> Arquivo Alterado: ${file}";
+        Console.WriteLine(logLine);
+        logLines.Add(logLine);
+        
         continue;
     }
     
-    Console.WriteLine($"👉🏻 Arquivo Nao Alterado: ${file}");
+    logLine = $" ==> Arquivo Nao Alterado: ${file}";
+    Console.WriteLine(logLine);
+    logLines.Add(logLine);
 }
 
-Console.WriteLine($"✅ Arquivos Encontrados: {numberOfFiles}");
-Console.WriteLine($"✅ Arquivos Alterados: {numberofModifiedFiles}");
+logLines.Sort();
+File.WriteAllLines("./EtiquetaDensidade.log.txt", logLines);
+
+Console.WriteLine($" ==> Arquivos Encontrados: {numberOfFiles}");
+Console.WriteLine($" ==> Arquivos Alterados: {numberofModifiedFiles}");
 
 Console.WriteLine();
 Console.WriteLine("Pressione qualquer tecla para sair...");
